2fbe8eef58d5dadb1693d6f87ba286ecd5e1f51b
Vladimir Pushkarev <slide58.motmom@gmail.com>
2019-10-03 16:21:31 +0300

    try 5000 port

--- sioserver.py
+++ sioserver.py
@@ -53,4 +53,4 @@ app.router.add_static('/static', 'static')
 app.router.add_get('/', index)
 
 if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=8080)
+    web.run_app(app, host='0.0.0.0', port=5000)
